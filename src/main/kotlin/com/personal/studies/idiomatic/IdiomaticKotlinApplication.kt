151ab9ca1b63eebcf0a02fad1ad1b90ac8ec09c5
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2023-01-08 11:39:07 -0300

    Initial commit with default config

---
+++ src/main/kotlin/com/personal/studies/idiomatic/IdiomaticKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.personal.studies.idiomatic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdiomaticKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<IdiomaticKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/personal/studies/idiomatic/IdiomaticKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personal.studies.idiomatic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdiomaticKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
