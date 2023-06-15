e25a8304f26766057260582fdde0dddce9b8fffa
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2023-06-15 01:09:52 -0300

    Supervisor with exception handler and infinite sequence

--- src/main/kotlin/com/personal/studies/idiomatic/coroutines/Dispatcher.kt
+++ src/main/kotlin/com/personal/studies/idiomatic/coroutines/Dispatcher.kt
@@ -4,31 +4,48 @@ import kotlinx.coroutines.*
 
 fun main() {
     runBlocking {
+
         supervisorScope {
-            launch {
-                println(Thread.currentThread().name)
+            val handler = CoroutineExceptionHandler { _, exception ->
+                println("CoroutineExceptionHandler got $exception with suppressed ${exception.suppressed.contentToString()}")
+//                cancel("Stop The World", exception)
             }
 
-            launch(context = Dispatchers.Default) {
+            launch(handler) {
+                println(Thread.currentThread().name)
+                generateSequence(1L) { it * 2 }
+                    .forEach {
+                        println("The value is $it")
+                        delay(500)
+                    }
+            }
+
+
+            launch(handler) {
                 supervisorScope {
-                    for (i in 1..100) {
-                        println("Default $i")
+                    for (i in 1..1_000_0000) {
+                        if (i % 10_000 == 0)
+                            println("Default $i")
                         yield()
 
-                        if (i == 59) throw RuntimeException()
+                        if (i == 999_999) throw RuntimeException("Inject some failure $i")
                     }
                 }
             }
 
-            launch(context = Dispatchers.IO) {
-                for (i in 1..200) {
-                    println("IO $i")
+            launch(handler) {
+                for (i in 1..2_000_000) {
+                    if (i % 20_000 == 0)
+                        println("IO $i")
                     yield()
+
+                    if (i == 1_000_999) throw RuntimeException("Inject some failure $i")
+
                 }
             }
+
+        }.invokeOnCompletion {
+            println("All the jobs are done and it is $it")
         }
-//            .reinvokeOnCompletion {
-//            println("All the jobs are done and it is $it")
-//        }
     }
 }
